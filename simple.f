da557fdf9c9ccd05a79b579df24b12026640604b
Dave Goehrig <dave@dloh.org>
2025-11-20 13:50:43 +0100

    fixing text

--- simple.f
+++ simple.f
@@ -2,6 +2,6 @@ include curl.f
 0 value client
 curl_easy_init to client
 client 0= ABORT" failed to initialize curl"
-client CURLOPT_URL s" https://dloh.org/index.html" drop curl_easy_setopt 0= ABORT" Failed to set URL"
+client CURLOPT_URL s" https://dloh.org/index.html" drop curl_easy_setopt drop
 client curl_easy_perform 0<> ABORT" Failed to fetch simple webpage"
 bye
