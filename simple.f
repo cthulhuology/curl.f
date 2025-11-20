b1592f72286255484700b9daf9617767d9aad5f2
Dave Goehrig <dave@dloh.org>
2025-11-20 13:56:35 +0100

    fixing zero buffer bug

--- simple.f
+++ simple.f
@@ -1,7 +1,6 @@
 include curl.f
 0 value client
 curl_easy_init to client
-client 0= ABORT" failed to initialize curl"
-client CURLOPT_URL s" https://dloh.org/index.html" drop curl_easy_setopt drop
-client curl_easy_perform 0<> ABORT" Failed to fetch simple webpage"
+client CURLOPT_URL z" https://dloh.org" curl_easy_setopt drop
+client curl_easy_perform 0<> ABORT" Failed to fetch website "
 bye
