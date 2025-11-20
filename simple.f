9150c89b40a3da099ea83c028c30dfe9907aefb7
Dave Goehrig <dave@dloh.org>
2025-11-20 14:04:47 +0100

    adding some error handling

--- simple.f
+++ simple.f
@@ -1,7 +1,14 @@
 include curl.f
+
 0 value client
-curl_easy_init to client
-client CURLOPT_URL z" https://dloh.org" curl_easy_setopt drop
-client curl_easy_perform 0<> ABORT" Failed to fetch website "
-client curl_easy_cleanup
+
+: get-dloh.org
+	curl_easy_init to client
+	client CURLOPT_URL z" https://dloh.org" curl_easy_setopt drop
+	client curl_easy_perform ?dup if
+		curl_easy_strerror zcount type 
+	then
+	client curl_easy_cleanup ;
+
+get-dloh.org
 bye
